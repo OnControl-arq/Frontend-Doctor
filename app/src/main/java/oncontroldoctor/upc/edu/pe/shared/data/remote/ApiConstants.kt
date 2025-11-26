b17d36263767d99014de1cee8bb8955f54d0cba8
MQRF123 <u20221e734@upc.edu.pe>
2025-11-26 13:48:45 -0500

    Configuration

--- app/src/main/java/oncontroldoctor/upc/edu/pe/shared/data/remote/ApiConstants.kt
+++ app/src/main/java/oncontroldoctor/upc/edu/pe/shared/data/remote/ApiConstants.kt
@@ -1,8 +1,11 @@
 package oncontroldoctor.upc.edu.pe.shared.data.remote
 
 object ApiConstants {
-    //"https://oncontrolbackend-gtbdhpc9fgd2epdx.westus3-01.azurewebsites.net/api/v1/"
-    //wss://d656-38-187-27-247.ngrok-free.app/ws/websocket
-    const val BASE_URL = "https://oncontrolbackend-gtbdhpc9fgd2epdx.westus3-01.azurewebsites.net/api/v1/"
-    const val WS_BASE_URL = "https://oncontrolbackend-gtbdhpc9fgd2epdx.westus3-01.azurewebsites.net"
+    // Cambia la URL de Azure por la de tu localhost
+    // 10.0.2.2 es la IP especial para que el emulador de Android vea tu PC
+    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
+    const val WS_BASE_URL = "ws://10.0.2.2:8080"
+
+    // Si usas un dispositivo físico, usa tu IP real:
+    // const 2val BASE_URL = "http://192.168.1.X:8080/api/v1/"
 }

1861c8bfb1983fa6041c77028214bef6acd347f7
MQRF123 <u20221e734@upc.edu.pe>
2025-11-28 16:24:36 -0500

    Configuration

--- app/src/main/java/oncontroldoctor/upc/edu/pe/shared/data/remote/RetrofitProvider.kt
+++ app/src/main/java/oncontroldoctor/upc/edu/pe/shared/data/remote/RetrofitProvider.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitProvider {
     //"https://oncontrolbackend-gtbdhpc9fgd2epdx.westus3-01.azurewebsites.net/api/v1/"
-    const val BASE_URL = "https://oncontrolbackend-gtbdhpc9fgd2epdx.westus3-01.azurewebsites.net/api/v1/"
+    const val BASE_URL = "http://10.0.2.2:8080/api/v1/"
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
